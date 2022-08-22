d42d7fd5c3fcba6cb829b8a7f590871afe224805
HyoJun-A <wnsgy79@gmail.com>
2022-08-22 11:00:56 +0900

    data bar 최종 수정

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.post('/data', function(req, res){
     const population = "SELECT * FROM POPULATION WHERE Districtname = '" + user_data[0] + "';";
     const school = "SELECT * FROM SCHOOL WHERE Districtname = '" + user_data[0] + "';";
     // 테이터 퍼센트 값 
-    const data_num = "SELECT TRUNCATE( COUNT(Districtname) / (SELECT COUNT(Districtname) FROM cctv) *100, 0) as Cdata from cctv WHERE Districtname = '" + user_data[0] + "' UNION SELECT TRUNCATE( COUNT(Districtname) / (SELECT COUNT(Districtname) FROM population) *100, 0) as Pdata from population WHERE Districtname = '" + user_data[0] + "'UNION SELECT TRUNCATE( COUNT(Districtname) / (SELECT COUNT(Districtname) FROM school) *100, 0) as Sdata from school WHERE Districtname = '" + user_data[0] + "';";
+    const data_num = "SELECT TRUNCATE( COUNT(Districtname) / (SELECT COUNT(Districtname) FROM cctv) *100, 0) as Cdata from cctv WHERE Districtname = '" + user_data[0] + "' UNION SELECT TRUNCATE( COUNT(Districtname) / (SELECT COUNT(Districtname) FROM population) *100, 0) as Pdata from population WHERE Districtname = '" + user_data[0] + "'UNION SELECT TRUNCATE( COUNT(Districtname) / (SELECT COUNT(Districtname) FROM school) *100, 0) as Sdata from school WHERE Districtname = '" + user_data[0] + "'UNION ALL SELECT TRUNCATE(enter /(SELECT SUM(enter) FROM data)*100, 0) FROM data WHERE district_name = '" + user_data[0] +"' UNION ALL SELECT TRUNCATE(foreigner /(SELECT SUM(foreigner) FROM data)*100, 0) FROM data WHERE district_name = '" + user_data[0] + "'UNION ALL SELECT TRUNCATE(pol /(SELECT SUM(pol) FROM data)*100, 0) FROM data WHERE district_name = '" + user_data +"' ;"
     
     // 범죄율
     const crime = 
